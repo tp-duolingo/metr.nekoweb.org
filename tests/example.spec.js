434b72b040c53f52ddc5a8fbb56fd97f95ade643
Metr <cetinsoylusarp@gmail.com>
2025-04-17 23:38:31 +0300

    Bleh 2

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 test('hello world test', async ({ page }) => {
     await page.goto('http://localhost:3000');
     const title = await page.title();
-    expect(title).toBe('Expected Title');
+    expect(title).toBe("Metr's Website");
 });
